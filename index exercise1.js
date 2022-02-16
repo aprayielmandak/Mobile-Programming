1db5a65a5bc049d7ac28313231b2a6ab0b564bcf
mirandamaria31 <mirandacheliamaria@gmail.com>
2022-02-16 12:28:22 +0800

    commit2 exercise1

--- index exercise1.js
+++ index exercise1.js
@@ -35,3 +35,47 @@ let people = ["Greg", "Mary", "Devon", "James"];
 //     }console.log(people[i])
 // }
 
+// 8 using slice
+// peopleNew = people.slice(1);
+// console.log(peopleNew);
+
+//9 using splice
+// people.splice(2,1,"Elisabeth", "Artie")
+// console.log(people);
+
+// 10 use concat
+// let withBob = people.concat("Bob")
+// console.log(people);
+// console.log(withBob);
+
+// object exercise
+let programming = {
+    languages: ["JavaScript", "Python", "Ruby"],
+    isChallenging: true,
+    isRewarding: true,
+    difficulty: 8,
+    jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
+};
+// 1
+programming.languages.push("Go")
+console.log(programming);
+
+// 2
+programming['difficulty'] = 7;
+console.log(programming);
+
+//3
+delete programming.jokes;
+console.log(programming);
+
+//4
+programming.isFun = true;
+console.log(programming);
+
+//5 
+programming.languages.map(function(item){
+    for (i=0;i<programming.languages.length;i++)
+    {
+        console.log([i] + " - " + programming.languages[i])
+    }
+})
