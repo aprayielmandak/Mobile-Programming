48670f6d7f22c3940f65f814da4fa03c05871f87
mirandamaria31 <mirandacheliamaria@gmail.com>
2022-02-16 12:25:45 +0800

    commit1 exercise1

---
+++ index exercise1.js
@@ -0,0 +1,37 @@
+let people = ["Greg", "Mary", "Devon", "James"];
+// console.log(people)
+
+// 1 for loop
+// for (i=0;i<people.length;i++)
+// {
+//     console.log(people[i]);
+// }
+
+// 2 for-each
+// people.forEach(function(human){
+//     console.log(human);
+// });
+
+//3 remove greg
+// people.shift()
+// console.log (people);
+
+// 4 remove james
+// people.pop()
+// console.log(people);
+
+// 5 add matt
+// people.unshift("Matt")
+// console.log(people);
+
+// 6 add name
+// people.push("K4")
+// console.log(people);
+
+// 7 after console mary, exit from loop
+// for (i=0; i<people.length; i++){
+//     if (i>1){
+//         break;
+//     }console.log(people[i])
+// }
+
