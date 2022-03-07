536949de6aeff8193b7d6bf4a2e2653a73aef553
aprayielmandak <s21910554@student.unklab.ac.id>
2022-03-07 08:54:10 +0800

    tambah toyota

--- exercise6/Components/Toyota.js
+++ exercise6/Components/Toyota.js
@@ -1,19 +1,32 @@
 import React, {useState} from 'react';
-import {View} from 'react-native';
-import Toyota from './Components/Toyota';
+import {View, Text, Button, StyleSheet} from 'react-native';
 
-export default App = () => {
-  const [state, setState] = useState({
-   Toyota: {
-      pemilik : 'Aprayiel',
-      warna: 'gray',
-      alamat : 'Bitung',
+export default Toyota = ({props}) => {
+  const [jumlah5, rubahJumlah5] = useState(0);
+  const styles = StyleSheet.create({
+    container: {
+      margin :5,
+      backgroundColor: 'gray',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text1: {
+      color: 'lightpink',
+      fontWeight: 'bold',
+    },
+    text2: {
+      color: 'lightpink',
     },
   });
 
   return (
-    <View>
-      <Toyota props={state.Toyota} />
+    <View style={styles.container}>
+      <Text style={styles.text1}>
+        Hy, saya {props.pemilik} suka mobil berwarna {props.warna}
+      </Text>
+      <Button title="tambah" onPress={() => rubahJumlah5(jumlah5 + 1)} />
+      <Text style={styles.text2}>Jumlah mobil yang ada : {jumlah5}</Text>
+      <Button title="kurang" onPress={() => rubahJumlah5(jumlah5 - 1)} />
     </View>
   );
 };
